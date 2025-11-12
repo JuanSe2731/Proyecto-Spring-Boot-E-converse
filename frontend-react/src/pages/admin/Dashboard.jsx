2a3a36dac1fb0bda7e3a1cb5274f250cd0c276f1
Lucky <jhonandersonvargas6@gmail.com>
2025-11-11 22:05:30 -0500

    FrontEnd with React, config

---
+++ frontend-react/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,11 @@
+const AdminDashboard = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <h1 className="text-3xl font-bold text-center py-10">
+        Dashboard Admin - En construcción
+      </h1>
+    </div>
+  );
+};
+
+export default AdminDashboard;
